a3f05e447f334003735b17bc38fe4caa238f6a78
Naveen Dharni <naveen.dharni@gmail.com>
2021-06-03 18:02:46 +0530

    router: routing through link & programmatically

--- pages/client/index.js
+++ pages/client/index.js
@@ -1,10 +1,33 @@
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 const Client = () => {
   const router = useRouter();
   console.log(router.query);
+
+  const toHome = () => {
+    // router.push("/");
+    router.push({
+      pathname: "/",
+    });
+  };
+
   return (
     <div>
+      <li>
+        <Link href="/blog">Blog</Link>
+      </li>
+      <li>
+        <Link
+          href={{
+            pathname: "/client/[all]/[id]",
+            query: { all: 221, id: 4 },
+          }}
+        >
+          Client Id
+        </Link>
+      </li>
+      <button onClick={toHome}>Homepage</button>
       <p>This is a Client</p>
     </div>
   );
