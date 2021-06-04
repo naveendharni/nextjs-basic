1b78b21619636a1077e382f2597fb345edd84884
Naveen Dharni <naveen.dharni@gmail.com>
2021-06-04 15:05:58 +0530

    feat: added getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,47 @@
-const HomePage = () => {
+import path from "path";
+import fs from "fs/promises";
+
+function HomePage(props) {
+  console.log({ props });
+  const { products } = props;
   return (
-    <div>
-      <p>HomePage</p>
-    </div>
+    <ul>
+      {products.map((item) => (
+        <li key={item.id}>{item.title}</li>
+      ))}
+    </ul>
   );
-};
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  console.log({ filePath });
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data",
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      products: data.products,
+    },
+    // redirect: {
+    //   destination: "/blog",
+    // },
+    // notFound: true // when no data found you cann add
+  };
+}
 
 export default HomePage;
